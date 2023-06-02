7803af170df4e25eaa3fe62cb72093b5a8adce8f
Rezuan Ahmed <rezuan.ahmed.pro@gmail.com>
2023-06-02 07:49:46 +0600

    feat: user schema and model create

--- index.js
+++ index.js
@@ -4,6 +4,32 @@ const mongoose = require('mongoose');
 const app = express();
 const port = 4000;
 
+app.use(express.json());
+
+//create user schema
+const usersSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  age: {
+    type: Number,
+    required: true,
+  },
+  languages: [
+    {
+      type: String,
+      required: true,
+    },
+  ],
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+//create user Model
+const User = mongoose.model('Users', usersSchema);
+
 //connect with database
 const connectDB = async () => {
   try {
@@ -15,6 +41,22 @@ const connectDB = async () => {
     process.exit(1);
   }
 };
+
+//create users
+app.post('/users', async (req, res) => {
+  try {
+    const newUser = new User({
+      name: req.body.name,
+      age: req.body.age,
+      languages: req.body.languages,
+    });
+
+    //save to database
+    const userData = await newUser.save();
+
+    res.status(201).send(userData);
+  } catch (error) {}
+});
 
 app.get('/', (_req, res) => {
   res.send('<h1>Welcome to Home Page</h1>');
